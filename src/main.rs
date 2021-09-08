36a209e6c68482a5edc72e5d1dafda81f5bbe5b1
KingCakeTheFruity <maxim.gorishniy@gmail.com>
2021-09-09 01:00:14 +0300

    fixed bugs, dont work now

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ use bevy::{prelude::*};
 mod controller;
 use controller::{Controller, ControllerAction, KeyEvent, KeyEventType, KeyPressMode, binding_args::BindArg};
 
-const SCR_W: f32 = 1000.;
-const SCR_H: f32 = 800.;
+const SCR_W:     f32 = 1000.;
+const SCR_H:     f32 = 800.;
 const SCR_NAME: &str = "RE-FLEX";
 
 fn main() {
@@ -48,10 +48,9 @@ fn player_move(player: &mut LocalPlayer, _args: &CtrlArgs, key_mode: KeyPressMod
 }
 
 struct Name(String);
+struct Position(Vec3);
 
-struct LocalPlayer {
-    pos: Vec3,
-}
+struct LocalPlayer;
 
 struct CtrlArgs {
     _mouse_pos: Vec2,
@@ -73,43 +72,13 @@ fn setup(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
         .insert(Name("@pl".to_string()));
 }
 
-fn keyboard_event_system(mut controller: ResMut<Controller<LocalPlayer, CtrlArgs, KeyCode, BindArg>>, keys: Res<Input<KeyCode>>, mut query: Query<(&mut LocalPlayer, &mut Transform)>) {
+fn keyboard_event_system(mut controller: ResMut<Controller<LocalPlayer, CtrlArgs, KeyCode, BindArg>>, keys: Res<Input<KeyCode>>) {
     for &key in keys.get_just_pressed() {
         controller.key_event(KeyEvent {key: key, etype: KeyEventType::PRESSED });
     }
 
     for &key in keys.get_just_released() {
         controller.key_event(KeyEvent {key: key, etype: KeyEventType::RELEASED });
-    }
-
-    // sorry for next 30 lines of code, they are just for comparison
-    
-    if keys.pressed(KeyCode::Up) {
-        for (mut entity, mut transform) in query.iter_mut() {
-            entity.pos += Vec3::new(0.,  5., 0.);
-            transform.translation = entity.pos;
-        }
-    }
-
-    if keys.pressed(KeyCode::Down) {
-        for (mut entity, mut transform) in query.iter_mut() {
-            entity.pos += Vec3::new(0.,  -5., 0.);
-            transform.translation = entity.pos;
-        }
-    }
-
-    if keys.pressed(KeyCode::Left) {
-        for (mut entity, mut transform) in query.iter_mut() {
-            entity.pos += Vec3::new(-5.,  0., 0.);
-            transform.translation = entity.pos;
-        }
-    }
-
-    if keys.pressed(KeyCode::Right) {
-        for (mut entity, mut transform) in query.iter_mut() {
-            entity.pos += Vec3::new(5.,  0., 0.);
-            transform.translation = entity.pos;
-        }
     }
 }
 
